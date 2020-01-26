4560ab36045c12b7a8f66543bc9900e01fb22f1b
Elvin Samuel <Elvin@USATestPrep.com>
2020-01-25 21:20:56 -0500

    Modify sample data, reformat renderQuestion(), populateQuestionAndAnswerFields(), and resetAnswerColors().

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
 $(document).ready(function() {
     var sampleData = {
-        'A' : ['A', 'B', 'C', 'D'],
-        'B' : ['E', 'F', 'G', 'H'],
-        'C' : ['I', 'J', 'K', 'L'],
-        'D' : ['M', 'N', 'O', 'P']
+        '彼' : ['機', '器', '装', '彼'],
+        '赤' : ['炎', '尖', '赤', '輪'],
+        '郭' : ['郭', '翼', '気', '付'],
+        '置' : ['帰', '置', '孤', '独']
     }
 
     function renderQuestion() {
+        populateQuestionAnswerFields();
+        resetAnswerColors();
+    }
+
+    function populateQuestionAnswerFields() {
         var questionAndAnswers = getRandomQuestionAndAnswersData();
-        alert(questionAndAnswers['question']);
+        var question = questionAndAnswers['question'];
+        var answerArray = questionAndAnswers['answersArray']; 
+
+        $('#mainContent').text(question);
+        $('#answer_a').text(answerArray[0]);
+        $('#answer_b').text(answerArray[1]);
+        $('#answer_c').text(answerArray[2]);
+        $('#answer_d').text(answerArray[3]);
     }
 
     function getRandomQuestionAndAnswersData() {
@@ -55,6 +67,14 @@ $(document).ready(function() {
             'background-color' : 'green',
             'color' : '#EEE',
         });
+    }
+
+    function resetAnswerColors() {
+        $("li[id^='answer_']").css({
+            'background-color' : '#DDD',
+            'color' : '#000',
+            'opacity' : '1.0'
+        })
     }
     
     function validateAnswerChoice(answer, answerChoiceID) {
